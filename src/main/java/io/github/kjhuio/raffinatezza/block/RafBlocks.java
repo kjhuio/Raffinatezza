245ba48c6a4e27faa913be7a9316634f206f44c8
qwe0412 <qwe0412.mc@gmail.com>
2026-06-06 14:50:30 +0900

    feat(balloon): add shears support and wool-like properties

--- src/main/java/io/github/kjhuio/raffinatezza/block/RafBlocks.java
+++ src/main/java/io/github/kjhuio/raffinatezza/block/RafBlocks.java
@@ -16,37 +16,37 @@ import java.util.function.Supplier;
 public class RafBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Raffinatezza.MODID);
 
-    public static final DeferredBlock<Block> BLACK_BALLOON = registerBlock("black_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).requiresCorrectToolForDrops().sound(SoundType.WOOL)));
+    public static final DeferredBlock<Block> BLACK_BALLOON = registerBlock("black_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> BLUE_BALLOON = registerBlock("blue_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> BLUE_BALLOON = registerBlock("blue_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> BROWN_BALLOON = registerBlock("brown_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> BROWN_BALLOON = registerBlock("brown_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> CYAN_BALLOON = registerBlock("cyan_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> CYAN_BALLOON = registerBlock("cyan_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> GRAY_BALLOON = registerBlock("gray_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> GRAY_BALLOON = registerBlock("gray_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> GREEN_BALLOON = registerBlock("green_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> GREEN_BALLOON = registerBlock("green_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> LIGHT_BLUE_BALLOON = registerBlock("light_blue_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> LIGHT_BLUE_BALLOON = registerBlock("light_blue_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> LIGHT_GRAY_BALLOON = registerBlock("light_gray_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> LIGHT_GRAY_BALLOON = registerBlock("light_gray_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> LIME_BALLOON = registerBlock("lime_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> LIME_BALLOON = registerBlock("lime_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> MAGENTA_BALLOON = registerBlock("magenta_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> MAGENTA_BALLOON = registerBlock("magenta_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> ORANGE_BALLOON = registerBlock("orange_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> ORANGE_BALLOON = registerBlock("orange_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> PINK_BALLOON = registerBlock("pink_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> PINK_BALLOON = registerBlock("pink_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> PURPLE_BALLOON = registerBlock("purple_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> PURPLE_BALLOON = registerBlock("purple_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> RED_BALLOON = registerBlock("red_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> RED_BALLOON = registerBlock("red_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> WHITE_BALLOON = registerBlock("white_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> WHITE_BALLOON = registerBlock("white_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
-    public static final DeferredBlock<Block> YELLOW_BALLOON = registerBlock("yellow_balloon",() -> new Block(BlockBehaviour.Properties.of()));
+    public static final DeferredBlock<Block> YELLOW_BALLOON = registerBlock("yellow_balloon",() -> new Block(BlockBehaviour.Properties.of().strength(0.8f).sound(SoundType.WOOL)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name,Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name,block);
